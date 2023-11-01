16cef625f838c1a52b60e6aea18f55d2ed26384e
Bedirhan <bedirhansaricayir@gmail.com>
2023-11-02 00:36:30 +0300

    first internal sharing released

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,6 @@
+import java.util.Properties
+import java.io.FileInputStream
+
 plugins {
     id("com.android.application")
     kotlin("android")
@@ -6,6 +9,10 @@ plugins {
     id("com.google.gms.google-services")
     id("com.google.firebase.crashlytics")
 }
+
+val keystorePropertiesFile = rootProject.file("keystore.properties")
+val keystoreProperties = Properties()
+keystoreProperties.load(FileInputStream(keystorePropertiesFile))
 
 android {
     namespace = ConfigData.applicationId
@@ -21,10 +28,25 @@ android {
         testInstrumentationRunner = ConfigData.testInstrumentationRunner
     }
 
+    signingConfigs {
+        create("release") {
+            keyAlias = keystoreProperties["keyAlias"] as String
+            keyPassword = keystoreProperties["keyPassword"] as String
+            storeFile = file(keystoreProperties["storeFile"] as String)
+            storePassword = keystoreProperties["storePassword"] as String
+        }
+    }
+
+
+
     buildTypes {
         getByName("release") {
+            signingConfig = signingConfigs.getByName("release")
             isMinifyEnabled = false
-            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
 
         }
     }
@@ -45,55 +67,55 @@ android {
     }
 
     composeOptions {
-        kotlinCompilerExtensionVersion  = Versions.kotlinCompilerExtensionVersion
+        kotlinCompilerExtensionVersion = Versions.kotlinCompilerExtensionVersion
     }
 }
 
 dependencies {
     coreLibraryDesugaring(Dependencies.desugaring)
 
-    implementation (Dependencies.coreKtx)
-    implementation (platform(Dependencies.platformKotlin))
-    implementation (Dependencies.lifecycleRuntimeKtx)
-    implementation (Dependencies.compose)
-    implementation (platform(Dependencies.platformCompose))
-    implementation (Dependencies.composeUi)
-    implementation (Dependencies.composeUiGraphics)
-    implementation (Dependencies.composeUiPreview)
-    implementation (Dependencies.material3)
-    implementation (Dependencies.foundation)
+    implementation(Dependencies.coreKtx)
+    implementation(platform(Dependencies.platformKotlin))
+    implementation(Dependencies.lifecycleRuntimeKtx)
+    implementation(Dependencies.compose)
+    implementation(platform(Dependencies.platformCompose))
+    implementation(Dependencies.composeUi)
+    implementation(Dependencies.composeUiGraphics)
+    implementation(Dependencies.composeUiPreview)
+    implementation(Dependencies.material3)
+    implementation(Dependencies.foundation)
 
-    testImplementation (Dependencies.junit)
-    androidTestImplementation (Dependencies.junitTest)
-    androidTestImplementation (Dependencies.espressoCore)
-    androidTestImplementation (platform(Dependencies.composeTest))
-    androidTestImplementation (Dependencies.composeTestJunit4)
-    debugImplementation (Dependencies.composeTestTooling)
-    debugImplementation (Dependencies.composeTestManifest)
+    testImplementation(Dependencies.junit)
+    androidTestImplementation(Dependencies.junitTest)
+    androidTestImplementation(Dependencies.espressoCore)
+    androidTestImplementation(platform(Dependencies.composeTest))
+    androidTestImplementation(Dependencies.composeTestJunit4)
+    debugImplementation(Dependencies.composeTestTooling)
+    debugImplementation(Dependencies.composeTestManifest)
 
-    implementation (Dependencies.composeViewModel)
-    implementation (Dependencies.ktxViewModel)
+    implementation(Dependencies.composeViewModel)
+    implementation(Dependencies.ktxViewModel)
 
-    implementation (Dependencies.coroutinesCore)
-    implementation (Dependencies.coroutinesAndroid)
+    implementation(Dependencies.coroutinesCore)
+    implementation(Dependencies.coroutinesAndroid)
 
-    implementation (Dependencies.retrofit)
-    implementation (Dependencies.gsonConvertor)
-    implementation (Dependencies.okHttp)
-    implementation (Dependencies.okHttpInterceptor)
+    implementation(Dependencies.retrofit)
+    implementation(Dependencies.gsonConvertor)
+    implementation(Dependencies.okHttp)
+    implementation(Dependencies.okHttpInterceptor)
 
-    implementation (Dependencies.navigation)
+    implementation(Dependencies.navigation)
 
-    implementation (Dependencies.splash)
+    implementation(Dependencies.splash)
 
-    implementation (Dependencies.hiltAndroid)
-    ksp (Dependencies.hiltAndroidCompiler)
-    implementation (Dependencies.hiltNavigationCompose)
-    ksp (Dependencies.hiltCompiler)
+    implementation(Dependencies.hiltAndroid)
+    ksp(Dependencies.hiltAndroidCompiler)
+    implementation(Dependencies.hiltNavigationCompose)
+    ksp(Dependencies.hiltCompiler)
 
-    implementation (Dependencies.datastore)
+    implementation(Dependencies.datastore)
 
-    implementation (Dependencies.numberPicker)
+    implementation(Dependencies.numberPicker)
 
     implementation(platform(Dependencies.platformFirebase))
     implementation(Dependencies.firebaseAuth)
--- buildSrc/src/main/java/ConfigData.kt
+++ buildSrc/src/main/java/ConfigData.kt
@@ -3,8 +3,8 @@ object ConfigData {
     val compileSdk = 34
     val mindSdk = 24
     val targetSdk = 34
-    val versionCode = 1
-    val versionName = "1.0"
+    val versionCode = 2
+    val versionName = "1.0.2"
     val applicationId = "com.lifting.app"
     val testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 }
