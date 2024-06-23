3cdd0b9e67345c0f5049685bb74d48e9e97a2340
bedirhan <bedirhansaricayir@gmail.com>
2024-06-24 00:42:27 +0300

    created base module

---
+++ core/base/build.gradle.kts
@@ -0,0 +1,37 @@
+plugins {
+    id(BuildPlugins.ANDROID_LIBRARY_PLUGIN)
+    id(BuildPlugins.KOTLIN_ANDROID_PLUGIN)
+}
+
+android {
+    namespace = "com.lifting.app.core.base"
+    compileSdk = AppConfig.COMPILE_SDK
+
+    defaultConfig {
+        minSdk = AppConfig.MIN_SDK
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+dependencies {
+    api(Kotlin.ktxViewModel)
+}
---
+++ core/base/src/main/java/com/lifting/app/core/base/viewmodel/BaseViewModel.kt
@@ -0,0 +1,63 @@
+package com.lifting.app.core.base.viewmodel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharedFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.receiveAsFlow
+import kotlinx.coroutines.launch
+
+internal abstract class BaseViewModel<STATE : State, EVENT : Event, EFFECT : Effect> : ViewModel() {
+
+    private val initialState: STATE by lazy { setInitialState() }
+
+    abstract fun setInitialState(): STATE
+
+    private val _state: MutableStateFlow<STATE> = MutableStateFlow(initialState)
+    val state: StateFlow<STATE> = _state.asStateFlow()
+
+    private val _event: MutableSharedFlow<EVENT> = MutableSharedFlow()
+    val event: SharedFlow<EVENT> = _event.asSharedFlow()
+
+    private val _effect = Channel<EFFECT>()
+    val effect = _effect.receiveAsFlow()
+
+    fun getCurrentState() = state.value
+
+    init {
+        subscribeToEvents()
+    }
+
+    abstract fun handleEvents(event: EVENT)
+
+    private fun subscribeToEvents() {
+        viewModelScope.launch {
+            _event.collect {
+                handleEvents(it)
+            }
+        }
+    }
+
+    fun setState(state: STATE) {
+        viewModelScope.launch { _state.emit(state) }
+    }
+
+    fun setEvent(event: EVENT) {
+        viewModelScope.launch { _event.emit(event) }
+    }
+
+    fun setEffect(effect: EFFECT) {
+        viewModelScope.launch { _effect.send(effect) }
+    }
+
+}
+
+
+internal interface State
+internal interface Event
+internal interface Effect
